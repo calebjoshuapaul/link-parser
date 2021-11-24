fe891b84c8041cb89e090897cbe61a25d2c12d34
Caleb Joshua <43238940+calebjoshuapaul@users.noreply.github.com>
2021-11-24 12:43:42 +0530

    create get link list template and append links to ul as li elements

--- script.js
+++ script.js
@@ -4,10 +4,21 @@
 
 const fetchLinksBttn = document.querySelector("#fetchLinksBttn");
 const urlInput = document.querySelector("#url");
+const result = document.querySelector("#result-list");
 
 fetchLinksBttn.addEventListener("click", async function (event) {
   event.stopPropagation();
   const url = urlInput.value;
   const response = await fetch("https://api.mapmyquest.net/?url=" + url);
-  console.log(response);
+
+  let links = await response.json();
+  result.innerHTML = getLinksListTemplate(links);
 });
+
+function getLinksListTemplate(links) {
+  return links
+    .map((link) => {
+      return `<li><a href="${link}">${link}</a></li>`;
+    })
+    .join("");
+}
